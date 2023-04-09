558933a72ef63ce7e030eea5da7e6b31bb2a9427
Rohan Raj <76591097+rajrohan10899@users.noreply.github.com>
2023-04-09 19:32:56 +0530

    Added solution - LeetHub

---
+++ Stock buy and sell - GFG/stock-buy-and-sell.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+//User function template for C++
+
+class Solution{
+public:
+    //Function to find the days of buying and selling stock for max profit.
+    vector<vector<int> > stockBuySell(vector<int> A, int n){
+        vector<vector<int>> ans;  int CP=INT_MAX,currInd=-1;
+        for(int i=0;i<n;i++){
+            
+            if(CP>A[i]){
+                currInd = i;
+                CP = A[i] ; 
+            }
+            else if(CP<A[i]){ 
+                ans.push_back({currInd , i});
+            //    cout<< currInd << " " << i <<endl;
+                
+                CP = A[i];
+                currInd= i;
+            }
+        }
+         
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int check(vector<vector<int>> ans, vector<int> A, int p)
+{
+    int c = 0;
+    for(int i=0; i<ans.size(); i++)
+        c += A[ans[i][1]]-A[ans[i][0]];
+    return (c==p) ? 1 : 0;
+}
+
+int main()
+{   
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> A(n);
+        for (int i=0; i<n; ++i){
+            cin>>A[i];
+        }
+        Solution ob;
+        vector<vector<int> > ans = ob.stockBuySell(A, n);
+        int p = 0;
+        for(int i=0; i<n-1; i++)
+        {
+            int x = A[i+1]-A[i];
+            if(x>0)
+                p += x;
+        }
+        if(ans.size()==0)
+            cout<<"No Profit";
+        else{
+            cout<<check(ans,A,p);
+        }cout<<endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
