7311c811f9c3416c4cbb3fe82878f3be9b534f2f
Rohan Raj <76591097+rajrohan10899@users.noreply.github.com>
2023-04-29 00:22:29 +0530

    Added solution - LeetHub

--- Kadane's Algorithm - GFG/kadanes-algorithm.cpp
+++ Kadane's Algorithm - GFG/kadanes-algorithm.cpp
@@ -12,18 +12,29 @@ class Solution{
     long long maxSubarraySum(int arr[], int n){
         
         // Your code here
-        int sum = 0;
-        int maxi = arr[0];
+         long long maxi = LONG_MIN; // maximum sum
+         long long sum = 0;
 
-        for(int i = 0; i<n; i++) {
-          sum  = sum + arr[i];
+        for (int i = 0; i < n; i++) {
 
-          maxi = max(maxi, sum);
+            sum += arr[i];
 
-          if(sum < 0) 
+        if (sum > maxi) {
+            maxi = sum;
+        }
+
+        // If sum < 0: discard the sum calculated
+        if (sum < 0) {
             sum = 0;
         }
-        return maxi;
+    }
+
+    // To consider the sum of the empty subarray
+    // uncomment the following check:
+
+    //if (maxi < 0) maxi = 0;
+
+    return maxi;
         
     }
 };
